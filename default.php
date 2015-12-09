abe4e89d68421ad0d6c1c1c972671798317bf94e
Akuma no Kami <Akuma no Kami>
2015-12-09 18:17:25 +0400

    ajout de la premiere page

---
+++ default.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	 <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <title>All Cheat Code</title>
+    </head>
+ <!--[if IE 6 ]><body class="ie6 old_ie"><![endif]-->
+    <!--[if IE 7 ]><body class="ie7 old_ie"><![endif]-->
+    <!--[if IE 8 ]><body class="ie8"><![endif]-->
+    <!--[if IE 9 ]><body class="ie9"><![endif]-->
+    <!--[if !IE]><!--><body><!--<![endif]-->
+        <div id="bloc_page">
+           <?php include("head.php") ?>
+           <div class="page">
+            <section>
+                <article>
+                    <h1><img src="images/news.jpg" alt="News" class="ico_categorie" /></h1>
+                    <p> --> Code pour les AC mis en ligne</p>
+                    <p> --> J'ai trouvé des codes pour Watch Dogs masi je suis pas sur qu'ils fonctionnent</p>
+                    <p> --> Le site est un peu vide pour le moment mais il se rempliras petit a petit.<br />
+					 Je conte aussi créer un forum pour que l'on puisse discuter mais plus tard :)
+                    </p>
+                    <p> --> Je remercie grandement mon graphiste, car sans lui l'aparence du site laisserais à désirer. <br />
+                     Que se soit l'image de fond ou celle du haut de la page, sans lui je serais loin d'avoir ce résultat :)</p>
+                     <p> --> Beaucoup de projets peu de temps il va falloir faire vite mais bien aussi ;)</p>
+                     <p> --> Ahah enfin première page replie de code fini :D<br />
+                     J'ai eu du mal comme je code tout il y a 1447 lignes de codes x.x j'ai perdu mes yeux. </p>
+                </article>
+                <aside>
+                    <h1>Chronologie</h1>
+                    <img src="images/bulle.png" alt="" id="fleche_bulle" />
+                    
+                    <p>
+                    Dans ce petit article je vais mettre toute la chronologie du site de ça mise en ligne a la sortie de chaque page.
+                    </p>
+                </aside>
+            </section>
+            
+            <?php include("foot.php") ?>
+            </div>
+        </div>
+    </body>
+</html>
